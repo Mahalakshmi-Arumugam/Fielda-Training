6cee0d76afefe626d8b6c938c9bddb7b27d108c2
Mahalakshmi-Arumugam <maha18aaru@gmail.com>
2023-11-28 22:17:04 +0530

    Null safety and Methods

---
+++ Day-8/Methods/classMethod.dart
@@ -0,0 +1,10 @@
+class Sample{
+   var name = "Mahalakshmi";
+   static void printName(name){
+      print(name);
+   }
+}
+
+void main(){
+   Sample.printName("Mahalakshmi Arumugam");
+}
---
+++ Day-8/Methods/instanceMethod.dart
@@ -0,0 +1,11 @@
+class Name{
+   var name = "Mahalakshmi";
+   void printName(name){
+      print(name);
+   }
+}
+
+void main(){
+   Name myName = new Name();
+   myName.printName("Mahalakshmi Arumugam");
+}
