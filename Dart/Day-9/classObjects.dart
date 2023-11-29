3d82dbd80ed0f396dfaf6c6dc75bbf25d3d1cb57
Mahalakshmi-Arumugam <maha18aaru@gmail.com>
2023-11-29 12:09:40 +0530

    Dart completed

--- Dart/Day-9/classObjects.dart
+++ Dart/Day-9/classObjects.dart
@@ -1,26 +1,26 @@
 //Declaring a class and an Object In Dart
-    class Bicycle {
-      String? color;
-      int? size;
-      int? currentSpeed;
-    
-      void changeGear(int newValue) {
-        currentSpeed = newValue;
-      }
-    
-      void display() {
-        print("Color: $color");
-        print("Size: $size");
-        print("Current Speed: $currentSpeed");
-      }
-    }
+class Bicycle {
+  String? color;
+  int? size;
+  int? currentSpeed;
 
-    void main(){
-        // Here bicycle is object of class Bicycle. 
-        Bicycle bicycle = Bicycle();
-        bicycle.color = "Red";
-        bicycle.size = 26;
-        bicycle.currentSpeed = 0;
-        bicycle.changeGear(5);
-        bicycle.display();
-    }
+  void changeGear(int newValue) {
+    currentSpeed = newValue;
+  }
+
+  void display() {
+    print("Color: $color");
+    print("Size: $size");
+    print("Current Speed: $currentSpeed");
+  }
+}
+
+void main() {
+  // Here bicycle is object of class Bicycle.
+  Bicycle bicycle = Bicycle();
+  bicycle.color = "Red";
+  bicycle.size = 26;
+  bicycle.currentSpeed = 0;
+  bicycle.changeGear(5);
+  bicycle.display();
+}
--- Dart/Day-9/dartEnumeration.dart
+++ Dart/Day-9/dartEnumeration.dart
@@ -1,23 +1,24 @@
-enum EnumofYear {   
-January,  
-February,  
-March,  
-April,  
-May,  
-June,  
-July,  
-August,  
-September,  
-October,  
-November,  
-December,  
-}  
-void main() {    
-   print(EnumofYear.values);  
-   EnumofYear.values.forEach((v) => print('value: $v, index: ${v.index}'));  
+enum EnumofYear {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
 
-   //we can also use for loop for this
-   for (var v in EnumofYear.values ) {
+void main() {
+  print(EnumofYear.values);
+  EnumofYear.values.forEach((v) => print('value: $v, index: ${v.index}'));
+
+  //we can also use for loop for this
+  for (var v in EnumofYear.values) {
     print('value: $v, index: ${v.index}');
-   }
-} 
+  }
+}
